54dc3a6fdea92b25c2dc81488f06e3872dcac1d2
Akib13 <akibahmed@iut-dhaka.edu>
2021-02-14 15:54:09 +0600

    Updates

---
+++ precache-manifest.46ce2336870e2754b1bc237dfc9af341.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "452df523c31715bfce9ce70ec5e4f112",
+    "url": "/react-chess/index.html"
+  },
+  {
+    "revision": "10d3592551473697207b",
+    "url": "/react-chess/static/css/main.b4156784.chunk.css"
+  },
+  {
+    "revision": "29ad0ba469aae406ac24",
+    "url": "/react-chess/static/js/2.ac8c2480.chunk.js"
+  },
+  {
+    "revision": "5b571a43117d7de158db9e221eaa0225",
+    "url": "/react-chess/static/js/2.ac8c2480.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10d3592551473697207b",
+    "url": "/react-chess/static/js/main.9adaba85.chunk.js"
+  },
+  {
+    "revision": "7bf79dabeab6448c2b67",
+    "url": "/react-chess/static/js/runtime-main.0cd08013.js"
+  },
+  {
+    "revision": "3382a95341b72c229183dcc0f1168176",
+    "url": "/react-chess/static/media/q_w.3382a953.png"
+  }
+]);
